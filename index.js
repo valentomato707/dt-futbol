9f5451b77e8c5249118cf3f52a56c7cd23109666
Vale <vale@gmail.com>
2024-05-08 09:58:30 -0300

    HTTP status

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const port = 3000
 const serverPath = `http://localhost:${port}`
 const players = require('./3-jugador/jugador')
 
-app.get('/', (req, res) => (res.send('Hello, world!')))
-app.get('/players', (req, res) => (res.send(players.jugadores())))
+app.get('/', (req, res) => (res.status(200).send('Hello, world!')))
+app.get('/players', (req, res) => (res.status(200).send(players.jugadores())))
+app.get('*', (req, res) => (res.status(404).send('Something went wrong. Route not found.')))
 
 app.listen(port, () => (console.log(`Server running on port 3000: ${serverPath}`)))
